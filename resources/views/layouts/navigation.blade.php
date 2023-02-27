4cc0e8181d237e0fb2e7c9b10362c1f958c8bad4
Jichul54 <kjc778869@gmail.com>
2023-02-27 17:35:05 +0900

    Procfile for Heroku

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
+                        <img src="{{ asset('assets/bのみ.png') }}" class="block h-9 w-auto fill-current text-gray-800" />
                 </div>
 
                 <!-- Navigation Links -->
